cfbec6cab6c8c20ebefc250fcc819d42b0925343
leenk <kassisleen@gmail.com>
2017-08-01 17:40:51 +0300

    m

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 # SQLAlchemy
-from model import Base, YourModel
+from model import Base, User, Video, Comment
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -14,6 +14,144 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/')
-def hello_world():
-    return render_template('register.html')
+@app.route('/', methods=['GET','POST'])
+def register():
+    #If it's a simple get, return the wanted page.
+    if request.method == 'GET':
+        return render_template('register.html')
+    
+    #If the user pressing register button it will try to sign him up
+    #and return "Login page".   
+    else:
+        username      = request.form.get('username')
+        name          = request.form.get('fullname')
+        password      = request.form.get('password')
+        email         = request.form.get('email')
+        country       = request.form.get('country')
+        sing          = request.form.get('sing')
+        guitar        = request.form.get('guitar')
+        drums         = request.form.get('drums')
+        flute         = request.form.get('flute')
+
+        #If this username is already exists it will bring you back.
+        if len(session.query(User).filter_by(username = username).all()) != 0:
+            return redirect(url_for('register'))
+        
+        #If it's a new one create a new user and go to the "Login" page
+        else:            
+            user = User(username = username, name = name, password = password, email = email, country = country, sing = sing, guitar = guitar, drums = drums, flute = flute)
+            session.add(user)
+            session.commit()
+
+            return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    #Checking if it's a real username and real password
+    else:
+        check_username      = request.form.get('check_username')
+        check_password      = request.form.get('check_password')
+
+        wanted_user         = session.query(User).filter_by(username = check_username).first()
+
+        #if it is correct continue to the homepage
+        if wanted_user.password is check_password:
+            return redirect(url_for('home',user_id = wanted_user.id))
+        #Ekse try again
+        else:
+            return redirect(url_for('login'))
+
+@app.route('/home/<int:user_id>')
+def home(user_id):
+
+    #Publishing on the feed all the videos and also saving the user (NOTE : there is a need?)
+    user = session.querry(User).filter_by(id = user_id).first()
+
+    videos = session.querry(Video).all()
+
+    return render_template('home.html', user = user, vidoes = videos)
+
+@app.route('/profile/<int:user_id>')
+def profile(user_id):
+
+    #Showing the whole videos of the current user and showing all the info about him.
+    user = session.querry(User).filter_by(id = user_id).first()
+
+    videos = session.querry(Video).filter_by(poster = user.name).all()
+
+    return render_template('profile.html', user = user, videos = videos)
+
+@app.route('/friends/<int:user_id>')
+def friends(user_id):
+
+    #Showing the current user friend list
+    user = session.querry(User).filter_by(id = user_id).first()
+    return render_template('friends.html', user = user)
+
+@app.route('/publish/<int:user_id>', methods=['GET','POST'])
+def publish(user_id):
+    videos = session.querry(Video).all()
+    user = session.querry(User).filter_by(id = user_id).first()
+    if request.method == 'GET':
+        return render_template('publish.html', user = user)
+
+    #Creare new video to publish
+    else:
+        video         = request.form.get('video')
+        description   = request.form.get('description')
+        rating        = request.form.get('rating')
+        owner         = user_id
+
+        if video in videos:
+            return redirect_url('publish',user_id = user_id)
+        else:    
+            new_vid = video(video = video, description = description, rating = rating, owner = owner)
+            session.add(new_vid)
+            session.commit()
+
+            return redirect_url('home',user_id = user_id)
+
+@app.route('/Add/<int:user_id>/<int:video_id>', methods=['GET','POST'])
+def add(user_id, video_id):
+
+    #Taking the old video ti present it to the user and also to mention the video last publisher to post it
+    #Interactivly
+    user = session.querry(User).filter_by(id = user_id).first()
+    video = session.querry(Video).filter_by(id = video_id).first()
+
+    if request.method == 'GET':
+        return render_template('add.html', user_id = user_id, video_id = video_id)
+
+    #Adding the video with refrence to the old one
+    else:
+        video         = request.form.get('video')
+        description   = request.form.get('description')
+        rating        = request.form.get('rating')
+        owner         = user.id
+
+        other_video = video.id
+        publish = false        
+
+        new_vid = Videos(video = video, description = description, publish = publish, other_video = other_video, rating = rating, owner = owner)
+        session.add(new_vid)
+        session.commit()
+
+        return redirect_url('home',user_id = user_id)
+
+
+                                                ############
+                                                #          #
+                                                #  NOTE'S  #
+                                                #          #
+                                                ############
+#Ask an instructor if it's possible to create a linked list out of the friend list as a feature (MODEL).
+#Ask an instructor how to present an actual video (FRONTEND).
+#Think about a more efficiant way to store the data (MODEL).
+#FEATURE : create out own icon's to put insted of boring button like a picture button(CSS).
+#Ask an instructor to help backend team to debug and understand if the code is good.
+#Ask an instructor if it's possible to make a kind of a "scroll".
+
--- model.py
+++ model.py
@@ -1,12 +1,39 @@
-from sqlalchemy import Column, DateTime, Integer, String, Boolean
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class YourModel(Base):
-    __tablename__  = 'yourmodel'
-    id             = Column(Integer, primary_key=True)
+class User(Base):
+    __tablename__ = 'User'
+    id            = Column(Integer, primary_key=True)
+    username      = Column(String(30))
+    name          = Column(String(30))
+    password      = Column(String(30))
+    email         = Column(String(30))
+    country       = Column(String(30))
+    sing          = Column(Boolean, default = False)
+    guitar        = Column(Boolean, default = False)
+    drums         = Column(Boolean, default = False)
+    flute         = Column(Boolean, default = False)
+
+
+class Video(Base):
+    __tablename__ = 'Video'
+    id            = Column(Integer, primary_key=True)
+    user_id      = Column(Integer, ForeignKey("User.id"))
+    video         = Column(String(140))
+    description   = Column(String(140))
+    publish       = Column(Boolean, default = True)
+    other_video   = Column(Integer, default = 0)
+    rating        = Column(Integer, default = 0)
+    owner         = Column(Integer, default = 0)
     # ADD YOUR FIELD BELOW ID
 
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id            = Column(Integer, primary_key=True)
+    video_id      = Column(Integer, ForeignKey("Video.id"))
+    video         = Column(Integer, default = 0)
+    comment       = Column(String(140))
 # IF YOU NEED TO CREATE OTHER TABLE 
 # FOLLOW THE SAME STRUCTURE AS YourModel
